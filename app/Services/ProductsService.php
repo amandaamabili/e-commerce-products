38fec33ffd67065cf4b724a9db80244f2e88d8c7
amandaamabili <amanda_amabilly.b@hotmail.com>
2022-05-27 23:06:11 -0300

    Create telescop and route store

--- app/Services/ProductsService.php
+++ app/Services/ProductsService.php
@@ -2,7 +2,15 @@
 
 namespace App\Services;
 
+use App\Repository\ProductRepository;
+
 class ProductsService
 {
-
+    protected  $repository;
+    public function __construct(ProductRepository $productRepository){
+        $this->repository = $productRepository;
+    }
+    public function  createNewProduct(array $data){
+        return $this->repository->createNewProduct($data);
+    }
 }
