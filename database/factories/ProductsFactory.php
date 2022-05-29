42b8a96d60d359681ca27329312b77a127eff6de
amandaamabili <amanda_amabilly.b@hotmail.com>
2022-05-29 15:51:45 -0300

    Create seeders

--- database/factories/ProductsFactory.php
+++ database/factories/ProductsFactory.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Database\Factories;
+use App\Models\Products;
 use Faker\Generator as Faker;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
@@ -15,12 +16,15 @@ class ProductsFactory extends Factory
      *
      * @return array<string, mixed>
      */
+    protected $model = Products::class;
+
+
     public function definition()
     {
         return [
-            'name' =>  $this->faker->name(),
+            'name' =>  $this->faker->word(),
             'sugest_price' => $this->faker->numberBetween($min = 1, $max = 50000),
-            'category' => $this->faker->unique()->randomElement(array("electronic", "tecnology", "house","culture_and_hoby",
+            'category' => $this->faker->randomElement(array("electronic", "tecnology", "house","culture_and_hoby",
                 "style_and_wellness", "auto" )),
             'brand' =>  $this->faker->word(),
             'stock_quantity' => $this->faker->randomDigitNotNull()
